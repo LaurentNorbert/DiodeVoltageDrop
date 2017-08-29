054ec4d7dc89cc1d0aa7f26f1f93909df7e4f130
Elbe <laurent.benaroya@gmail.com>
2017-08-29 23:08:35 +0200

    Compute Vdiode(Va-Vc) and Idiode = Vc/R in the setup section. The loop is section is empty.

--- sketch_diode_voltage_drop.ino
+++ sketch_diode_voltage_drop.ino
@@ -1,6 +1,6 @@
 /**************************************************************************/
 /*! 
-    @file     find diode voltae
+    @file     find diode voltage
     @author   Adafruit Industries // ELB
     @license  BSD (see license.txt)
 
@@ -20,13 +20,10 @@
 #include <Adafruit_MCP4725.h>
 
 Adafruit_MCP4725 dac;
-int sensorValue, sensorThres;
-bool checkVoltage;
+int sensorValue;
 uint32_t counter;
+float R = 1000; // 1k Ohms resistor
 
-// check if the arduino is computing using a blinking led
-int blinkPin = 13; // Pin 13 has an LED connected on most Arduino boards.
-bool onblink;
 
 void setup(void) {
   Serial.begin(9600);
@@ -36,63 +33,23 @@ void setup(void) {
   // For MCP4725A0 the address is 0x60 or 0x61
   // For MCP4725A2 the address is 0x64 or 0x65
   dac.begin(0x60);
-  checkVoltage = true; // init - compute voltage drop in loop
-  sensorThres = 20; // minimum value for diode on (voltage = sensorThres/1024.*5.)
-  Serial.println("Detecting diode voltage drop");
-  
-  // on board led
-  onblink = false;
-  pinMode(blinkPin, OUTPUT); 
-  digitalWrite(blinkPin, LOW); 
+
+  Serial.println("Diode voltage drop : ");
+
+  // Run through the full 12-bit scale analog output until the diode is on.
+  for (counter = 0; counter < 4096; counter += 16 )
+  {
+    dac.setVoltage(counter, false); // set analog output      
+    sensorValue = analogRead(A0); // read analog input
+
+    float Va = counter/4096.*5.; // 12 bits analog output
+    float Vc = sensorValue/1024.*5.; // 10 bits analog input
+    float Vd = Va-Vc;
+       
+    Serial.print("Vd = "); Serial.print(Vd);
+    Serial.print(", Id = "); Serial.println(Vc/R*1000);
+  }
+      
 }
 
-void loop(void) {
-    
-  if (checkVoltage)
-  {
-    // Run through the full 12-bit scale analog output until the diode is on.
-    for (counter = 0; counter < 4096; counter += 16 )
-    {
-      dac.setVoltage(counter, false); // set analog output
-      
-      sensorValue = analogRead(A0); // read analog input
-      //Serial.println(sensorValue);
-      if (sensorValue > sensorThres)
-      {
-        float up = counter/4096.*5.; // 12 bits analog output
-        float down = sensorValue/1024.*5.; // 10 bits analog input
-        float voltage = up-down;
-        Serial.print("Diode voltage drop : ");
-        Serial.print(voltage);
-        Serial.println(" V");
-        checkVoltage = false;
-        break;
-      }
-      
-      // make on board pin 13 blink
-      if (counter % 128 == 0)
-      {
-        onblink = !onblink;
-        if (onblink)
-          digitalWrite(blinkPin, HIGH);
-        else
-          digitalWrite(blinkPin, LOW);
-      }
-      delay(100);
-    }
-    if ((counter >= 4095) && (checkVoltage))
-    {
-      Serial.println("Error, maximum voltage reached");
-      //checkVoltage = false;
-      //digitalWrite(blinkPin, LOW);
-    }
-    checkVoltage = false;
-    digitalWrite(blinkPin, LOW);
-    
-    // wait 1 sec and set analog output to 0
-    delay(1000);
-    dac.setVoltage(0, false); // set analog output
-  }
-  else
-    delay(100);
-}
+void loop(void) {}
